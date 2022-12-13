5b1697e8236a10547cd3f04575100bf76a4ee6f8
Vladimir <fardz198727@gmail.com>
2022-12-13 11:18:37 +0300

    first commit

---
+++ Seminar4.2/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+
+int number = ReadInt("Введите число: ");
+
+int count = MethodOfCountingDigitsInNumber(number);
+
+MethodOfDisplayingSumOfDigitsInNumber(number, count);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int MethodOfCountingDigitsInNumber(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a = a / 10;
+        index++;
+    }
+    return index;
+}
+
+void MethodOfDisplayingSumOfDigitsInNumber (int n, int count)
+{
+    int sum = 0;
+    for (int i = 1; i <= count; i++)
+    {
+        sum = n % 10 + sum;
+        n = n / 10;
+    }
+    Console.WriteLine(sum);
+}
---
+++ Seminar4.3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+
+int[] NewArray(){
+Console.Write("Введите колличество чисел в массиве:\t");
+int numbers = int.Parse(Console.ReadLine());
+
+int[] array = new int[numbers];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"\n Введите число для заполнения массива {i}:\t");
+    array[i] = int.Parse(Console.ReadLine());
+    
+}
+Console.WriteLine("[{0}]", string.Join(", ", array));
+return array;
+}
+ int[] resalt = NewArray();
+ 
